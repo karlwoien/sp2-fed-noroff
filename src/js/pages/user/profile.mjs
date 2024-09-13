d32cef1f2e80ad8b496779e069b8730858ce738b
karlwoien <karlwoien@gmail.com>
2024-09-13 13:43:48 +0200

    updates profile.mjs to call renderUsingListings

--- src/js/pages/user/profile.mjs
+++ src/js/pages/user/profile.mjs
@@ -1,8 +1,13 @@
 import { profileInfo } from "../../components/profileInfo.mjs";
+import { renderUserListings } from "../../components/userListings.mjs";
 
 export async function loadProfile() {
   const profileContainer = document.querySelector("#profile-info");
   profileContainer.innerHTML = "";
 
+  // Load the profile info
   profileContainer.appendChild(await profileInfo());
+
+  // Load the user's listings
+  await renderUserListings();
 }
