5dc42ef4214db77d217850eac0b8694d668be460
karlwoien <karlwoien@gmail.com>
2024-09-16 13:24:52 +0200

    updates function to call renderUserBidHistory

--- src/js/pages/user/profile.mjs
+++ src/js/pages/user/profile.mjs
@@ -1,5 +1,6 @@
 import { profileInfo } from "../../components/profileInfo.mjs";
 import { renderUserListings } from "../../components/userListings.mjs";
+import { renderUserBidHistory } from "../../components/userBidHistory.mjs";
 
 export async function loadProfile() {
   const profileContainer = document.querySelector("#profile-info");
@@ -8,6 +9,9 @@ export async function loadProfile() {
   // Load the profile info
   profileContainer.appendChild(await profileInfo());
 
-  // Load the user's listings
+  // Load the users listings
   await renderUserListings();
+
+  // Load the users bid history
+  await renderUserBidHistory();
 }
